925e4059b31c4475580c40e1692d564d84e5330c
kpakozz96pyc <kozz2010@gmail.com>
2025-08-04 23:14:27 +0200

    offstream: added camera movement and fps count

--- src/camera.rs
+++ src/camera.rs
@@ -1,67 +1,105 @@
-use bevy::math::Vec3;
-use bevy::prelude::*;
+use bevy::app::{App, Startup};
+use bevy::input::mouse::{MouseButton, MouseMotion, MouseWheel};
+use bevy::math::{Quat, Vec3};
+use bevy::prelude::{ButtonInput, Camera3d, Commands, EventReader, KeyCode, Query, Res, Time, Transform, Update, With};
 
 pub struct CustomCameraPlugin;
 
-impl Plugin for CustomCameraPlugin {
-    fn build(&self, app: &mut App){
+impl bevy::prelude::Plugin for CustomCameraPlugin {
+    fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_camera)
-            .add_systems(Update, camera_controls);
-
-        return;
+            .add_systems(Update, (camera_controls, camera_zoom, camera_rotate));
     }
 }
 
-fn spawn_camera(mut commands: Commands){
+fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(-5.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-    )).insert(Name::new("Camera"));
+    ));
 }
 
-fn camera_controls(
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut camera_query: Query<&mut Transform, With<Camera3d>>,
-    time: Res<Time>
-){
-    let mut camera_transform = camera_query.single_mut().unwrap();
-    let forward = Vec3::new(camera_transform.forward().x, 0.0, camera_transform.forward().z);
-    let right = Vec3::new(camera_transform.right().x, 0.0, camera_transform.right().z);
+fn camera_controls(keyboard: Res<ButtonInput<KeyCode>>, mut camera_query: Query<&mut Transform, With<Camera3d>>, time: Res<Time>){
+    let mut camera = camera_query.single_mut().unwrap();
+
+    let forward = Vec3::new(camera.forward().x, camera.forward().y, camera.forward().z).normalize();
+    let right = Vec3::new(camera.right().x, camera.right().y, camera.right().z).normalize();
 
     let speed = 5.0;
     let rotation_speed = 1.5;
-
     let delta = time.delta_secs();
 
     let mut movement = Vec3::ZERO;
 
-    if  keyboard.pressed(KeyCode::KeyW)
-    {
+    if keyboard.pressed(KeyCode::KeyW) {
         movement += forward;
     }
-    if  keyboard.pressed(KeyCode::KeyS)
-    {
+    if keyboard.pressed(KeyCode::KeyS) {
         movement -= forward;
     }
-    if  keyboard.pressed(KeyCode::KeyD)
-    {
-        movement += right;
-    }
-    if  keyboard.pressed(KeyCode::KeyA){
+    if keyboard.pressed(KeyCode::KeyA) {
         movement -= right;
     }
-
-    if movement != Vec3::ZERO{
-        movement = movement.normalize()*speed*delta;
-        camera_transform.translation += movement;
+    if keyboard.pressed(KeyCode::KeyD) {
+        movement += right;
+    }
+    if keyboard.pressed(KeyCode::Space) {
+        movement += Vec3::Y;
     }
 
-    if keyboard.pressed(KeyCode::KeyQ){
-        camera_transform.rotate(Quat::from_axis_angle(Vec3::Y, rotation_speed*delta));
-    }
-    if keyboard.pressed(KeyCode::KeyE){
-        camera_transform.rotate(Quat::from_axis_angle(Vec3::Y, -rotation_speed*delta));
+    if movement != Vec3::ZERO {
+        movement = movement.normalize() * speed * delta;
+        camera.translation += movement;
     }
 
-    return;
+
+    if keyboard.pressed(KeyCode::KeyQ) {
+        let rotation = bevy::math::Quat::from_rotation_y(rotation_speed * delta);
+        camera.rotate(rotation);
+    }
+    if keyboard.pressed(KeyCode::KeyE) {
+        let rotation = Quat::from_rotation_y(-rotation_speed * delta);
+        camera.rotate(rotation);
+    }
+}
+
+fn camera_zoom(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut camera_query: Query<&mut Transform, With<Camera3d>>,
+    time: Res<Time>,
+) {
+    let mut camera_transform = camera_query.single_mut().unwrap();
+    let zoom_speed = 10.0;
+    let delta = time.delta_secs();
+
+
+    for event in mouse_wheel_events.read() {
+
+        let scroll_amount = event.y;
+
+        let zoom_direction = camera_transform.forward();
+        camera_transform.translation += zoom_direction * scroll_amount * zoom_speed * delta;
+    }
+}
+
+fn camera_rotate(
+    mouse_button: Res<ButtonInput<MouseButton>>,
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut camera_query: Query<&mut Transform, With<Camera3d>>,
+) {
+
+    if mouse_button.pressed(MouseButton::Middle) {
+        let mut camera_transform = camera_query.single_mut().unwrap();
+        let rotation_speed = 0.005;
+        for event in mouse_motion_events.read() {
+            let mouse_delta = event.delta;
+            let y_rotation = Quat::from_rotation_y(-mouse_delta.x * rotation_speed);
+            camera_transform.rotate(y_rotation);
+            let right_dir = Vec3::new(camera_transform.right().x, camera_transform.right().y, camera_transform.right().z);
+            let x_rotation = Quat::from_axis_angle(right_dir, -mouse_delta.y * rotation_speed);
+            camera_transform.rotate(x_rotation);
+        }
+    } else {
+        mouse_motion_events.clear();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod camera;
 use bevy::app::App;
 use bevy::prelude::*;
 use bevy::color::{Color, Srgba};
-use bevy::prelude::{Camera3d, ClearColor, Commands};
+use bevy::prelude::{ClearColor};
 use bevy_inspector_egui::bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use crate::camera::CustomCameraPlugin;
@@ -15,7 +15,7 @@ use crate::projectile::ProjectilePlugin;
 use crate::target::TargetPlugin;
 use crate::tower::TowerPlugin;
 use crate::world::WorldPlugin;
-
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
 
 fn main() {
@@ -24,6 +24,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(TowerPlugin)
         .add_plugins(WorldPlugin)
         .add_plugins(ProjectilePlugin)
